6ab7558975fdf19f2ac6556671a7f1f5f25cda4f
beenasree <36693763+beenasree@users.noreply.github.com>
2018-03-09 11:57:48 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bean_Brew_Pjt.Models;
+using System.IO;
+
+using System.Web;
+
+
+
+
+namespace Bean_Brew_Pjt.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Bean_BrewEntities db = new Bean_BrewEntities();
+
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        public ActionResult Home()
+        {
+            return View(db.Products.ToList());
+        }
+        //
+        // GET: /Product/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /Product/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Product/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product, HttpPostedFileBase file)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                string path = Path.Combine(Server.MapPath("~/Upload"), Path.GetFileName(file.FileName));
+                file.SaveAs(path);
+                product.image = "Upload/" + file.FileName;
+
+
+
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        //
+        // GET: /Product/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // POST: /Product/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /Product/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //
+        // POST: /Product/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
